d78a5e689f6fb2fcbd1c0e873f8210a5398abce7
zhengj3 <zhengj3@a6048abc-ed69-11dd-a550-c997509ed985>
2009-02-23 23:02:45 +0000

    comment,class,search, individual

--- trunk/connect_db.php
+++ trunk/connect_db.php
@@ -2,7 +2,7 @@
 
 $username=""; /* Your MySQL username/password goes here! */
 $password="";
-$database="RPH";
+$database="";
 
 mysql_connect(localhost,$username,$password);
 @mysql_select_db($database) or die( "Unable to select database, change username, password, database in connect_db.php");
