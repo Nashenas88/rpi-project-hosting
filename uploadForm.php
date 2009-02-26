06b67716ddb63f2d1ebbebb94418d66c2205ffc0
Nashenas88 <Nashenas88@a6048abc-ed69-11dd-a550-c997509ed985>
2009-02-26 06:02:59 +0000

    Modified uploadForm.php however still issues with uploadFiles.php only changed and changed back thines in menu and priviledge.php, added database name RPH to connect_db.php

--- uploadForm.php
+++ uploadForm.php
@@ -1,30 +1,16 @@
 
 <table width="500" border="0" cellpadding="0" cellspacing="0" bgcolor="#FFFFFF">
 <tr><td>
-<form name="upload" method="post" action="uploadFiles.php">
+<form name="upload" method="post" action="uploadFiles.php" enctype="multipart/form-data">
 <fieldset>
 <legend><strong>Upload</strong></legend>
 <table width="490" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
 
 <tr>
-<td>Choose a file to upload</td>
-<td>:</td>
-<td><input name="uploaded" type="file" /></td>
-</tr>
-
-<tr>
-<td>Choose a picture to upload</td>
-<td>:</td>
-<td><input name="uploadedPic" type="file" /></td>
-</tr>
-
-<p style="font-size:12px">By uploading files you certify that you have the right to distribute these files and that it does not violate the <a href="http://www.youtube.com/watch?v=oHg5SJYRHA0">Terms of Service</a>.</p>
-
-<tr>
 <td>Project Name</td>
 <td>:</td>
 <td><input name="projectName" type="text" id="projectName" />
-<input type="hidden" name="MAX_FILE_SIZE" value="1000000" /></td>
+</td>
 </tr>
 
 <tr>
@@ -59,10 +45,31 @@
 </tr>
 
 <tr>
+<td>Choose a file to upload</td>
+<td>:</td>
+<td><input name="file" type="file" /></td>
+</tr>
+
+<!--<tr>
+<td>Choose a picture to upload</td>
+<td>:</td>
+<td><input name="uploadedPic" type="file" /></td>
+</tr>!-->
+
+<tr>
 <td>&nbsp;</td>
 <td>&nbsp;</td>
 <td><input type="submit" name="Submit" value="Upload"</td>
-</tr></table>
+</tr>
+
+<tr>
+<td colspan="3">
+<p style="font-size:12px">By uploading files you certify that you have the right to distribute these files and that it does not violate the <a href="http://www.youtube.com\
+/watch?v=oHg5SJYRHA0">Terms of Service</a>.</p>
+</td>
+</tr>
+
+</table>
 </fieldset>
 </form>
 </td></tr>
