190eaa7a9230d17dab7145a37df5daee4c2a0771
dan.meretzky <dan.meretzky@a6048abc-ed69-11dd-a550-c997509ed985>
2009-03-25 18:16:50 +0000

    Added remove project. Comments limited to 500 characters.

--- connect_db.php
+++ connect_db.php
@@ -1,8 +1,8 @@
 <?php
 
-$user=""; /* Your MySQL username/password goes here! */
-$pass="";
-$database="RPH";
+$user="root"; /* Your MySQL username/password goes here! */
+$pass="75867586";
+$database="projecthost";
 
 mysql_connect(localhost,$user,$pass);
 mysql_select_db($database) or die( "Unable to select database, change username, password, database in connect_db.php");
