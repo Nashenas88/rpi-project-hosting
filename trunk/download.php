d9d76660a28d3c42164d25c918f6680132cb52f3
Nashenas88 <Nashenas88@a6048abc-ed69-11dd-a550-c997509ed985>
2009-03-25 18:14:42 +0000

    added some functionality to the download page

--- trunk/download.php
+++ trunk/download.php
@@ -4,4 +4,47 @@ session_start ();
 require ("connect_db.php");
 $id = mysql_real_escape_string ($_GET["id"]);
 
+$query = sprintf ("SELECT title, description, uploader, downloads,"
+		  . " size, project_location, authors, class, "
+		  . " major, school, date FROM projects WHERE id='%s';", $id);
+$result = mysql_query ($query);
+
+session_start ();
+require ("upper_header.php");
+
+if ($result)
+{
+  if (mysql_numrows ($result) > 0)
+  {
+    $row = mysql_fetch_assoc ($result);
+    echo $row['title'];
+    require ("lower_header.php");
+    require ("menu.php");
+    
+    echo "<br/><center><h1>" . htmlspecialchars ($row['title']) . "</h1></center><br />\n";
+    
+    $username = htmlspecialchars ($_SESSION['username']);
+    $path = $username . "/" . htmlspecialchars ($row['title']);
+    
+  }
+  else
+  {
+    echo "Error";
+    require ("lower_header.php");
+    require ("menu.php");
+    
+    echo "<br/><center>Project not found</center>";
+  }
+}
+else
+{
+  echo "Error";
+  require ("lower_header.php");
+  require ("menu.php");
+  
+  echo mysql_error($result);
+}
+
+
+
 ?>
