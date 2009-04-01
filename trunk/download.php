f7050a7403e2ce3883dc0a6b5f5796d85ee8daac
Nashenas88 <Nashenas88@a6048abc-ed69-11dd-a550-c997509ed985>
2009-04-01 18:30:35 +0000

    Hopefully download works now

--- trunk/download.php
+++ trunk/download.php
@@ -10,25 +10,41 @@ $query = sprintf ("SELECT title, description, uploader, downloads,"
 $result = mysql_query ($query);
 
 session_start ();
-require ("upper_header.php");
+#require ("upper_header.php");
 
 if ($result)
 {
   if (mysql_numrows ($result) > 0)
   {
     $row = mysql_fetch_assoc ($result);
-    echo $row['title'];
-    require ("lower_header.php");
-    require ("menu.php");
+    #echo $row['title'];
+    #require ("lower_header.php");
+    #require ("menu.php");
     
-    echo "<br/><center><h1>" . htmlspecialchars ($row['title']) . "</h1></center><br />\n";
+    #echo "<br/><center><h1>" . htmlspecialchars ($row['title']) . "</h1></center><br />\n";
     
     $username = htmlspecialchars ($_SESSION['username']);
-    $path = $username . "/" . htmlspecialchars ($row['title']);
+    $path = $username . "/" . $row['title'];
+    
+    require ("pclzip.lib.php");
+    
+    $zipfile = new PclZip ($row['title'] . "zip");
+    
+    $v_list = $zipfile->create ($path);
+    
+    if ($v_list == 0)
+    {
+	die ("Error: " . $zipfile->errorInfo (true));
+    }
+    
+    header ("Content-type: application/octet-stream");
+    header ("Content-disposition: attachment; filename=" . $row['title'] . ".zip");
+    readfile ($row['title'] . ".zip");
     
   }
   else
   {
+    require ("upper_header.php");
     echo "Error";
     require ("lower_header.php");
     require ("menu.php");
