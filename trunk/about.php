9a1e778aa4ebeb5a92d9a62e2932106ad062e8a6
tennismage <tennismage@a6048abc-ed69-11dd-a550-c997509ed985>
2009-04-01 15:10:30 +0000

    Updated about.php Still needs some work

--- trunk/about.php
+++ trunk/about.php
@@ -4,6 +4,34 @@ require ("upper_header.php");
 echo "About";
 require ("lower_header.php");
 require ("menu.php");
-echo "<br />\nThis website is a trial for the UI for the RPH project.";
+echo "<br />\n
+RPH<br>
+Pro<br>
+Ios<br>
+ jt<br>
+ ei<br>
+ cn<br>
+ tg<br>
+<br>
+<br>
+RPH is a project hosting site for RPI students. Students are able to log in to the website using their RCS IDs and Passwords.
+<br>
+Once logged in, students will be able to post their completed projects for everyone to see.
+<br>
+Projects may include anything from a CS1 class homework to a video game to a photograph.
+<br>
+<br>
+RPH was started as a project for the Software Design and Documentation class.
+<br>
+<br>
+Admins and Contributors:
+<br>
+Alex Mattern
+<br>
+Jin G Z
+<br>
+Dan 
+<br>
+Paul Faria";
 require ("footer.php");
 ?>
