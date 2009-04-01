ed197e4a63cc129e5b24bf2770b8f809c68b001c
tennismage <tennismage@a6048abc-ed69-11dd-a550-c997509ed985>
2009-04-01 13:56:08 +0000

    getPriviledge() function in Priviledge.php and Updated moderate.php so that you cannot ban yourself or someone of higher priviledge.

--- trunk/priviledge.php
+++ trunk/priviledge.php
@@ -11,6 +11,58 @@ function loggedIn ()
   return $_SESSION["loggedIn"];
 }
 
+
+
+/*
+function isModerator ()
+{
+  require("connect_db.php");
+  $username=$_SESSION['usrname'];
+
+  $priviledge_query= mysql_query("SELECT priviledge FROM users WHERE rcsid='".mysql_real_escape_string($username)."'");
+  $result = mysql_fetch_array($priviledge_query);
+
+  if($result[0] < 2)
+  {
+         return true;
+  }
+  else
+  {
+         return false;
+  } 
+}
+*/
+
+/***
+takes in one argument: username
+returns priviledge level of specified user
+0=admin, 1=moderator, 2=RPIuser, 3=unknown
+***/
+function getPriviledge ()
+{
+  // connect to database
+  require("connect_db.php");
+  $username=$_SESSION['username'];
+
+  $query = sprintf ("SELECT priviledge FROM users WHERE rcsid='%s'", mysql_real_escape_string($username));
+  $result = mysql_query ($query);
+    
+    if (!$result)
+    {
+        echo mysql_error ();
+        exit;
+    }
+    if (mysql_numrows ($result) > 0)
+    {        
+        return mysql_result($result, 0, 'priviledge');
+    }
+    else
+    {
+  	    echo "No Users... wtf";
+  	    return 3;
+    }
+}
+
 /***
 0=admin, 1=moderator, 2=RPIuser
 It should return true if user has moderator priveleges and false otherwise.
@@ -30,10 +82,7 @@ function isModerator ()
         exit;
     }
     if (mysql_numrows ($result) > 0)
-    {
-        #$result = mysql_fetch_array($priviledge_query);
-        #mysql_result($priviledge_query, 0, 'priviledge')
-        
+    {        
         if(mysql_result($result, 0, 'priviledge') < 2)
         {
     	    return true;
