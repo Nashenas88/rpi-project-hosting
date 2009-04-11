4b6cd1f827559a37b2b1e394c002bb9fe87405c7
Nashenas88 <Nashenas88@a6048abc-ed69-11dd-a550-c997509ed985>
2009-04-11 22:18:18 +0000

    Fixed more issues related to feater.php, only have moderate.php left =p

--- loginAlternate.php
+++ loginAlternate.php
@@ -1,11 +1,35 @@
 <?php
 session_start ();
-require ("upper_header.php");
-echo "Login";
-require ("lower_header.php");
-require ("menu.php");
-echo "<align=\"left\">\n<br />\n";
-require ("loginForm.php");
-echo "</align>";
-require ("footer.php");
+require ("feater.php");
+
+$output = "<align=\"left\">\n<br />\n";
+$output .= '<table width="310" border="0" cellpadding="0" cellspacing="0" bgcolor="#FFFFFF">';
+$output .= '<tr><td>';
+$output .= '<form name="login" method="post" action="checkLogin.php">';
+$output .= '<fieldset>';
+$output .= '<legend><strong>Login</strong></legend>';
+$output .= '<table width="300" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">';
+$output .= '<tr>';
+$output .= '<td>Username</td>';
+$output .= '<td>:</td>';
+$output .= '<td><input name="username" type="text" id="username" /></td>';
+$output .= '</tr>';
+$output .= '<tr>';
+$output .= '<td>Password</td>';
+$output .= '<td>:</td>';
+$output .= '<td><input name="pass" type="password" id="pass" /></td>';
+$output .= '</tr>';
+$output .= '<tr>';
+$output .= '<td>&nbsp;</td>';
+$output .= '<td>&nbsp;</td>';
+$output .= '<td><input type="submit" name="Submit" value="Login" /></td>';
+$output .= '</tr></table>';
+$output .= '</fieldset>';
+$output .= '</form>';
+$output .= '</td></tr>';
+$output .= '</table>';
+
+$output .= "</align>";
+
+make_page ("Login", $output);
 ?>
--- loginSuccess.php
+++ loginSuccess.php
@@ -1,15 +1,12 @@
 <?php
 session_start ();
-require ("upper_header.php");
-echo "Login Successful";
-require ("lower_header.php");
-require ("menu.php");
+require ("feater.php");
 
-echo "<br />\n<center>\n";
-echo "You have successfully logged in to RPI Project Hosting!\n";
-echo "<br />\n";
-echo "Click <a href=\"index.php\">here</a> to return to the main page.\n";
-echo "</center>";
+$output = "<br />\n<center>\n";
+$output .= "You have successfully logged in to RPI Project Hosting!\n";
+$output .= "<br />\n";
+$output .= "Click <a href=\"index.php\">here</a> to return to the main page.\n";
+$output .= "</center>";
 
-require ("footer.php");
+make_page ("Login Successful", $output);
 ?>
