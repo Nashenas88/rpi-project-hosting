c6d0d8c6ad89ad6911c626ed8bb38d05270b2994
Nashenas88 <Nashenas88@a6048abc-ed69-11dd-a550-c997509ed985>
2009-04-11 22:35:19 +0000

    fixed some issues with both

--- logoutSuccess.php
+++ logoutSuccess.php
@@ -1,16 +1,12 @@
 <?php
 session_start ();
-require ("upper_header.php");
-echo "Logout Successful";
-require ("lower_header.php");
-require ("menu.php");
+require ("feater.php");
  	
-$loggedIn = $_SESSION["loggedIn"];
-echo "<br />$loggedIn\n<center>\n";
-echo "You have successfully logged out of RPI Project Hosting!\n";
-echo "<br />";
-echo "Click <a href=\"index.php\">here</a> to return to the main page.\n";
-echo "</center>";
+$output .= "<br /><center>";
+$output .= "You have successfully logged out of RPI Project Hosting!";
+$output .= "<br />";
+$output .= "Click <a href=\"index.php\">here</a> to return to the main page.";
+$output .= "</center>";
  	
-require ("footer.php");
+make_page ("Logout Success", $output);
 ?>
--- moderate.php
+++ moderate.php
@@ -18,8 +18,8 @@ if (isModerator () == true)
 	$output .= "<form name='ban' method='POST' action='moderate.php'>";
 	$output .= "Username:&nbsp;&nbsp;&nbsp;&nbsp;<input name='username' id='input' type='text'\>";
 	$output .= "By:&nbsp;&nbsp;&nbsp;&nbsp;<select name='ban_unban'>";
-	$output .= "<option value="ban">Ban</option>";
-	$output .= "<option value="unban">Unban</option>";
+	$output .= "<option value=\"ban\">Ban</option>";
+	$output .= "<option value=\"unban\">Unban</option>";
 	$output .= "</select>";
 	$output .= "&nbsp;&nbsp;<input type='submit' value='Update'\>";
 	$output .= "</form>";
