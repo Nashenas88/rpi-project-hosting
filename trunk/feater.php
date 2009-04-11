57af9ac3a9d6cc4eb8e0a5115c44605e8b7ccfc1
Nashenas88 <Nashenas88@a6048abc-ed69-11dd-a550-c997509ed985>
2009-04-11 20:25:23 +0000

    Merged uploadForm and upload into one file, added menu into feater.php

--- trunk/feater.php
+++ trunk/feater.php
@@ -2,24 +2,27 @@
 
 function make_page ($title, $body)
 {
-  echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"\n';
-  echo '"http://www.w3.org/TR/html4/strict.dtd">\n';
-  echo '<html>\n';
-  echo '\n';
-  echo '<head>\n';
-  echo '<meta name="author" content="Paul Faria" />\n';
-  echo '<meta name="author" content="Alex Mattern" />\n';
-  echo '<meta name="author" content="Jin Zheng" />\n';
-  echo '<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />\n';
+  echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"';
+  echo '"http://www.w3.org/TR/html4/strict.dtd">';
+  echo '<html>';
+  echo '';
+  echo '<head>';
+  echo '<meta name="author" content="Paul Faria" />';
+  echo '<meta name="author" content="Alex Mattern" />';
+  echo '<meta name="author" content="Jin Zheng" />';
+  echo '<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />';
   echo '<title>RPI Project Hosting | ';
   echo $title;
-  echo '</title>\n';
-  echo '</head>\n';
-  echo '\n';
-  echo '<body>\n';
+  echo '</title>';
+  echo '</head>';
+  echo '';
+  echo '<body>';
+  
+  require ("menu.php");
+  
   echo $body;
-  echo '</body>\n';
-  echo '\n';
-  echo '</html>\n';
+  echo '</body>';
+  echo '';
+  echo '</html>';
 }
 ?>
