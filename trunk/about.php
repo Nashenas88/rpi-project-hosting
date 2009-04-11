4f85afac0087cc1d029a571e1f5765f9323c3b86
Nashenas88 <Nashenas88@a6048abc-ed69-11dd-a550-c997509ed985>
2009-04-11 22:08:13 +0000

    Fixed about.php

--- trunk/about.php
+++ trunk/about.php
@@ -1,34 +1,31 @@
 <?php
 session_start ();
-require ("upper_header.php");
-echo "About";
-require ("lower_header.php");
-require ("menu.php");
-echo "<br />\n";
-?>
-<img src="images/RPH.png" width='10%' height='10%'>
-<br>
-<br>
-RPH is a project hosting site for RPI students. Students are able to log in to the website using their RCS IDs and Passwords.
-<br>
-Once logged in, students will be able to post their completed projects for everyone to see.
-<br>
-Projects may include anything from a CS1 class homework to a video game to a photograph.
-<br>
-<br>
-RPH was started as a project for the Software Design and Documentation class.
-<br>
-<br>
-Admins and Contributors:
-<br>
-Alex Mattern
-<br>
-Jin Guang Zheng
-<br>
-Dan Meretzky
-<br>
-Paul Faria
 
-<?php
-require ("footer.php");
+require ("feater.php");
+
+$output = "<br />";
+$output .= "<img src=\"images/RPH.png\" width='10%' height='10%'>";
+$output .= "<br>";
+$output .= "<br>";
+$output .= "RPH is a project hosting site for RPI students. Students are able to log in to the website using their RCS IDs and Passwords.";
+$output .= "<br>";
+$output .= "Once logged in, students will be able to post their completed projects for everyone to see.";
+$output .= "<br>";
+$output .= "Projects may include anything from a CS1 class homework to a video game to a photograph.";
+$output .= "<br>";
+$output .= "<br>";
+$output .= "RPH was started as a project for the Software Design and Documentation class.";
+$output .= "<br>";
+$output .= "<br>";
+$output .= "Admins and Contributors:";
+$output .= "<br>";
+$output .= "Alex Mattern";
+$output .= "<br>";
+$output .= "Jin Guang Zheng";
+$output .= "<br>";
+$output .= "Dan Meretzky";
+$output .= "<br>";
+$output .= "Paul Faria";
+
+make_page ("About", $output);
 ?>
