c47bc5a0d423c674f3216fc5d66d4590243f5ce8
dan.meretzky <dan.meretzky@a6048abc-ed69-11dd-a550-c997509ed985>
2009-04-12 19:12:38 +0000

    remove useless files

--- trunk/priviledge.php
+++ trunk/priviledge.php
@@ -1,10 +1,8 @@
 <?php
-/***
+/*******************************************************************
 priviledge.php
-loggedIn()
-isModerator()
-Contains the priveledge reckoning functions
-***/
+Uses the getPriviledge() function to return the privilege of a user
+********************************************************************/
 
 function loggedIn ()
 {
@@ -12,31 +10,8 @@ function loggedIn ()
 }
 
 
-
-/*
-function isModerator ()
-{
-  require("connect_db.php");
-  $username=$_SESSION['usrname'];
-
-  $priviledge_query= mysql_query("SELECT priviledge FROM users WHERE rcsid='".mysql_real_escape_string($username)."'");
-  $result = mysql_fetch_array($priviledge_query);
-
-  if($result[0] < 2)
-  {
-         return true;
-  }
-  else
-  {
-         return false;
-  } 
-}
-*/
-
-/***
-takes in one argument: username
-returns priviledge level of specified user
-0=admin, 1=moderator, 2=RPIuser, 3=unknown
+// returns priviledge level of specified user
+// 0=admin, 1=moderator, 2=RPIuser, 3=unknown
 ***/
 function getPriviledge ()
 {
@@ -66,7 +41,7 @@ function getPriviledge ()
 /***
 0=admin, 1=moderator, 2=RPIuser
 It should return true if user has moderator priveleges and false otherwise.
-***/
+***
 function isModerator ()
 {
   // connect to database
@@ -96,5 +71,5 @@ function isModerator ()
     {
   	    echo "No Users... wtf";
     }
-}
+}*/
 ?>
