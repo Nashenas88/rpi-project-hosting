4f214dc732030f1141cd9a9af61f33f7f696cb53
dan.meretzky <dan.meretzky@a6048abc-ed69-11dd-a550-c997509ed985>
2009-04-12 18:37:02 +0000

    comments

--- trunk/login.php
+++ trunk/login.php
@@ -1,60 +1,40 @@
+/*******************************************************************
+login.php
+Allows user to login using their RPI username and password
+********************************************************************/
 <?php
-//
 
 // phpCAS simple client
-
-//
-
 /*******************************************
 Edit the CAS.php file to get rid of warnings. 
 Replace "/tmp" and "/tmp/" with a relative path like "./"
 *******************************************/
 
-
 // import phpCAS lib
-
 include_once('CAS-1.0.1/CAS.php');
-
 
 phpCAS::setDebug();
 
-
-
 // initialize phpCAS
-
 phpCAS::client(CAS_VERSION_2_0,'login.rpi.edu',443,'/cas');
 
-
-
 // no SSL validation for the CAS server
-
 phpCAS::setNoCasServerValidation();
 
-
-
 // force CAS authentication
-
 phpCAS::forceAuthentication();
 
-
-
 // at this step, the user has been authenticated by the CAS server
-
 // and the user's login name can be read with phpCAS::getUser().
 
-
-
 // logout if desired
-
 if (isset($_REQUEST['logout'])) {
     session_start ();
     session_destroy ();
    phpCAS::logout();
-
 }
 
-
-
+// get the username 
 $username = phpCAS::getUser ();
 
 require ("connect_db.php");
@@ -67,6 +47,7 @@ if (!$result)
   exit;
 }
 
+// check if user is banned
 $matches = mysql_numrows ($result);
 if ($matches > 0)
 {
@@ -89,6 +70,7 @@ if (!$result)
   exit;
 }
 
+// check the user does not exist in the database, add the user to the database
 $matches = mysql_numrows ($result);
 if ($matches == 0)
 {
@@ -96,7 +78,7 @@ if ($matches == 0)
     if (!mysql_query ($query))
     {
         require ("feater.php");
-	make_page ("Error", mysql_error ());
+		  make_page ("Error", mysql_error ());
         exit;
     }
 }
