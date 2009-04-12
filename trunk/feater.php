24e50988540f7e1c9a606b697b7dbfc6ea50c922
Nashenas88 <Nashenas88@a6048abc-ed69-11dd-a550-c997509ed985>
2009-04-12 22:15:58 +0000

    made some bug fixes

--- trunk/feater.php
+++ trunk/feater.php
@@ -7,29 +7,30 @@ foot() - prints the footer to the page
 
 function make_page ($title, $body)
 {
-  echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"';
-  echo '"http://www.w3.org/TR/html4/strict.dtd">';
-  echo '<html>';
-  echo '';
-  echo '<head>';
-  echo '<meta name="author" content="root"/>';
-  echo '<meta name="author" content="root"/>';
-  echo '<meta name="author" content="root"/>';
-  echo '<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />';
-  echo '<title>RPI Project Hosting | ';
-  echo $title;
-  echo '</title>';
-  echo '</head>';
-  echo '';
-  echo '<body background="images/background.jpg">';
-  
-  require ("menu.php");
-  
-  echo $body;
-  echo '<br /><br /><center>This page is licensed under the <a href=http://www.gnu.org/licenses/gpl.html>GNU General Public License v3</a></center><br />';
-  echo '</body>';
-  echo '';
-  echo '</html>';
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+"http://www.w3.org/TR/html4/strict.dtd">
+<html>
+
+<head>
+<meta name="author" content="root"/>
+<meta name="author" content="root"/>
+<meta name="author" content="root"/>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<title>RPI Project Hosting | <?php echo $title;?></title>
+</head>
+
+<body background="images/background.jpg">
+
+<?php
+require ("menu.php"); ?>
+echo $body;
+?>
+<br /><br /><center>This page is licensed under the <a href=http://www.gnu.org/licenses/gpl.html>GNU General Public License v3</a></center><br />
+</body>
+
+</html>
+<?php
 }
 
 // adds a header to the page
