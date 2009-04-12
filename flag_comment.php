49d33c4be8260203e99c5240abd09f3e546a6d7d
dan.meretzky <dan.meretzky@a6048abc-ed69-11dd-a550-c997509ed985>
2009-04-12 18:37:02 +0000

    comments

--- flag_comment.php
+++ flag_comment.php
@@ -1,22 +1,22 @@
+/*******************************************************************
+flag_comment.php
+Allows user to flag a comment, which sends an alert to a moderator
+********************************************************************/
 <?php
 
 session_start ();
 
 require("connect_db.php");
 
-/*
-* project id and user id need to query comment need to be flag
-*/
+// test if project id and user id exist
 if(isset($_REQUEST['project_id'])&&isset($_REQUEST['user_id']))
 {
-
+	// make variable text secure
 	$project_id=htmlspecialchars($_REQUEST['project_id']);
 	$user=htmlspecialchars($_REQUEST['user_id']);
 
 	
-	/*
-	*check if user comment exist
-	*/
+	// check if the comment exists
 	$query_comment="SELECT 1 FROM comments WHERE user_id='".mysql_real_escape_string($user)."' AND project_id=".mysql_real_escape_string($project_id);
 	$query_comment_res=mysql_query($query_comment);
 
@@ -30,9 +30,7 @@ if(isset($_REQUEST['project_id'])&&isset($_REQUEST['user_id']))
 	
 	$num_of_comment = mysql_numrows($query_comment_res);
 	
-	/*
-	*if comment exist update flag attribute
-	*/
+	// if comment exists, update flag attribute
 	if($num_of_comment>0)
 	{
 		$flag_comment="UPDATE comments SET flag=1 WHERE user_id='".mysql_real_escape_string($user)."' AND project_id=".mysql_real_escape_string($project_id);
@@ -51,11 +49,10 @@ if(isset($_REQUEST['project_id'])&&isset($_REQUEST['user_id']))
 	{
 		$_SESSION['message']= "<p>No such comment</p>";
 	}
-	
 }
 else
 {
-$_SESSION['message']="<p>Your comment flag fails</p>";
+$_SESSION['message']="<p>Your comment flag fails.</p>";
 }
 header("location:show_project.php?show_project_id=".$_REQUEST['project_id']);
 
