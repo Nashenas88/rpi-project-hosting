9797edddaf7223c608a59767e11878927be8ed0a
dan.meretzky <dan.meretzky@a6048abc-ed69-11dd-a550-c997509ed985>
2009-04-12 19:21:34 +0000

    remove ban_form

--- ban_form.php
+++
@@ -1,8 +0,0 @@
-$output .= "<form name='ban' method='POST' action='moderate.php'>";
-$output .= "Username:&nbsp;&nbsp;&nbsp;&nbsp;<input name='username' id='input' type='text'\>";
-$output .= "By:&nbsp;&nbsp;&nbsp;&nbsp;<select name='ban_unban'>";
-$output .= "<option value="ban">Ban</option>";
-$output .= "<option value="unban">Unban</option>";
-$output .= "</select>";
-$output .= "&nbsp;&nbsp;<input type='submit' value='Update'\>";
-$output .= "</form>";
