c2e67690d33001eee6fa8f3ae065f2bc3c1528d5
Nashenas88 <Nashenas88@a6048abc-ed69-11dd-a550-c997509ed985>
2009-04-14 02:25:22 +0000

    fixes to menu's n sh-tuff

--- trunk/upload.php
+++ trunk/upload.php
@@ -65,7 +65,49 @@ if (empty ($_SESSION['username']))
 <tr>
 <td>Related Major</td>
 <td>:</td>
-<td><input type=text name="projectMajor" size="20" /></td>
+<td><!--<input type=text name="projectMajor" size="20" />-->
+<select name="projectMajor">
+  <option value="ADMN">ADMN</option>
+  <option value="ARCH">ARCH</option>
+  <option value="ARTS">ARTS</option>
+  <option value="ASTR">ASTR</option>
+  <option value="BCBP">BCBP</option>
+  <option value="BIOL">BIOL</option>
+  <option value="BMED">BMED</option>
+  <option value="CHEM">CHEM</option>
+  <option value="CHME">CHME</option>
+  <option value="CIVL">CIVL</option>
+  <option value="COGS">COGS</option>
+  <option value="COMM">COMM</option>
+  <option value="CSCI">CSCI</option>
+  <option value="DSES">DSES</option>
+  <option value="ECON">ECON</option>
+  <option value="ECSE">ECSE</option>
+  <option value="ENGR">ENGR</option>
+  <option value="ENVE">ENVE</option>
+  <option value="EPOW">EPOW</option>
+  <option value="ERTH">ERTH</option>
+  <option value="IHSS">IHSS</option>
+  <option value="ISCI">ISCI</option>
+  <option value="ITEC">ITEC</option>
+  <option value="LGHT">LGHT</option>
+  <option value="LITR">LITR</option>
+  <option value="MANE">MANE</option>
+  <option value="MATH">MATH</option>
+  <option value="MATP">MATP</option>
+  <option value="MGMT">MGMT</option>
+  <option value="MTLE">MTLE</option>
+  <option value="PHIL">PHIL</option>
+  <option value="PHYS">PHYS</option>
+  <option value="PSYC">PSYC</option>
+  <option value="STSH">STSH</option>
+  <option value="STSS">STSS</option>
+  <option value="USAF">USAF</option>
+  <option value="USAR">USAR</option>
+  <option value="USNA">USNA</option>
+  <option value="WRIT">WRIT</option>
+</select>
+</td>
 </tr>
 
 <!-- Upload File -->
