5097a9297fea5e70d8cfc04332ade40015839122
tennismage <tennismage@a6048abc-ed69-11dd-a550-c997509ed985>
2009-04-15 16:18:37 +0000

    fixed bugs in moderate.php and priviledge.php

--- priviledge.php
+++ priviledge.php
@@ -4,9 +4,8 @@ priviledge.php
 Uses the getPriviledge() function to return the privilege of a user
 ********************************************************************/
 
-// returns priviledge level of specified user
+// returns priviledge level of SESSION current user
 // 0=admin, 1=moderator, 2=RPIuser, 3=unknown
-
 function getPriviledge ()
 {
   // connect to database
@@ -28,6 +27,33 @@ function getPriviledge ()
     else
     {
   	    echo "<br/><center>User " . $_SESSION['username'] . " does not exist</center><br/>";
+  	    return 3;
+    }
+}
+
+// takes the argument of a username
+// returns priviledge level of specified user
+// 0=admin, 1=moderator, 2=RPIuser, 3=unknown
+function getSpecifiedPriviledge ($user)
+{
+  // connect to database
+  require("connect_db.php");
+
+  $query = sprintf ("SELECT priviledge FROM users WHERE rcsid='%s'", mysql_real_escape_string($user));
+  $result = mysql_query ($query);
+    
+    if (!$result)
+    {
+        echo mysql_error ();
+        exit;
+    }
+    if (mysql_numrows ($result) > 0)
+    {        
+        return mysql_result($result, 0, 'priviledge');
+    }
+    else
+    {
+  	    echo "<br/><center>User " . $user . " does not exist.</center><br/>";
   	    return 3;
     }
 }
